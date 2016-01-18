4219729c7c05dcb8aeabb6c09f6141a6535609b6
Tomohiro Kumagai <tomohiro@ez-style.jp>
2016-01-19 02:11:23 +0900

    Initial Commit

---
+++ EzSample_GlobalLazyVariable/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EzSample_GlobalLazyVariable
+//
+//  Created by Tomohiro Kumagai on 1/19/16.
+//  Copyright © 2016 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
